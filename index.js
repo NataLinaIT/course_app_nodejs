e2e85b6a3d5b9c99f61fdd500c8daa2d4af7e891
NataLina <kalipsoN80@gmail.com>
2021-03-14 17:29:05 +0200

    update

--- index.js
+++ index.js
@@ -1,32 +1,70 @@
-const express = require('express')
-const path = require('path')
-const exphbs = require('express-handlebars')
-const homeRoutes = require('./routes/home')
-const cardRoutes = require('./routes/cart')
-const addRoutes = require('./routes/add')
-const coursesRoutes = require('./routes/courses')
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const homeRoutes = require("./routes/home");
+const cardRoutes = require("./routes/card");
+const addRoutes = require("./routes/add");
+const coursesRoutes = require("./routes/courses");
+const User = require("./models/user");
 
-const app = express()
+const app = express();
 
 const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: 'hbs'
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('604e25ac5b50ff1d249c6c00')
+    req.user = user
+    next()
+  } catch (e) {
+    console.log(e)
+  }
 })
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: true}))
+app.use("/", homeRoutes);
+app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
+app.use("/card", cardRoutes);
 
-app.use('/', homeRoutes)
-app.use('/add', addRoutes)
-app.use('/courses', coursesRoutes)
-app.use('/cart', cardRoutes)
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
+async function start() {
+  try {
+    const url =
+      "mongodb+srv://nch:rRF9ijUcRLBB1m8P@cluster0.6mwc1.mongodb.net/shop";
+    await mongoose.connect(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+    const candidate = await User.findOne();
+    if (!candidate) {
+      const user = new User({
+        email: "natalina@mail.com",
+        name: "NataLina",
+        cart: { items: [] },
+      });
+      await user.save();
+    }
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
